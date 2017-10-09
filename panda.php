ba3a105b8017a00a68d1dfeff7bd16700744835a
Elena Gridasova <rapid-steel@mail.ru>
2017-10-09 12:06:52 +0800

    --

--- panda.php
+++ panda.php
@@ -3,73 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <title>Panda rescue!</title>
-
-
-
-	<style>
-		body, html {
-			margin: 0;
-			padding: 0;
-		}
-
-		#outer-container {	text-align: center;	}
-		#game-container {	display: inline-block;}
-		#game-input {
-			position: absolute;
-			top: 0;
-			left: -200px;
-		}
-
-		#warning { display: none }
-
-		@media (max-width: 800px) {
-			#warning {
-				display: block;
-				margin: 5px 5%;
-				width: 90%;
-				padding: 15px;
-				color: grey;
-				font-family: "Verdana";
-				font-size: 16px;
-				text-align: justify;
-				text-align-all: center;
-				border: grey 2px dashed;
-				border-radius: 4px;
-			}
-		}
-		#excl-point {
-			position: relative;
-			float: left;
-			width: 30px;
-			height: 30px;
-			margin: 0 10px;
-			border: 5px orangered solid;
-			border-radius: 5px;
-		}
-		#excl-point div {
-			position: absolute;
-			top: -30px;
-			left: -20px;
-			width: 70px;
-			color: black;
-			font-size: 65px;
-			font-weight: 800;
-			text-align: center;
-		}
-
-		#close-button{
-			display: block;
-			margin: 5px auto;
-			padding: 10px 15px;
-			font: inherit;
-			color: grey;
-			border: 2px solid grey;
-			border-radius: 4px;
-			background-color: transparent;
-			cursor: pointer;
-		}
-
-	</style>
+	<link rel="stylesheet" href="css/panda-styles.css">
+	
 </head>
 <body>
 
